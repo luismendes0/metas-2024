430b3601e2352a4d4a27559a9cebcefaca5082fe
Luis Guilherme <105360460+luismendes0@users.noreply.github.com>
2024-03-21 08:31:46 -0300

    ok

--- main.js
+++ main.js
@@ -12,5 +12,5 @@ for(let i=0; i <botoes.length; i++){
         textos[i].classList.add("ativo");
     }
 }
-contadores[0].textContent = "Contagem Regressiva";
+contadores[0].textContent = "tempoObjetivo1";
 const tempoObjetivo1 = new Date("2030-02-21");
