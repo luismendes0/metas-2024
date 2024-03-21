ef945d9d6f4c2e2540c35e762204390f204caf58
Luis Guilherme <105360460+luismendes0@users.noreply.github.com>
2024-03-21 08:44:45 -0300

    ok

--- main.js
+++ main.js
@@ -14,4 +14,4 @@ for(let i=0; i <botoes.length; i++){
 }
 const tempoObjetivo1 = new Date("2030-02-21T00:00:00");
 let tempoAtual = new Date();
-contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
